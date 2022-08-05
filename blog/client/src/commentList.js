ebfd7ff53f652f644b69c48c451b70f54b62827c
uday <udaysaraswat2@gmail.com>
2022-08-05 22:58:25 +0530

    initial commit

---
+++ blog/client/src/commentList.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const CommentList = ({ comments }) => {
+  const renderedComment = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComment}</ul>;
+};
+
+export default CommentList;
---
+++ blog/event-bus/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/events', (req, res) => {
+  try {
+    const event = req.body;
+
+    axios.post('http://localhost:4000/events', event);
+    axios.post('http://localhost:4001/events', event);
+    axios.post('http://localhost:4002/events', event);
+
+    res.send({ status: 'OK' });
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+app.listen(4005, () => {
+  console.log('server is listening at 4005');
+});
